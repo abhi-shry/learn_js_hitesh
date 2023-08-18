745c73a00c2cc419224f3f0e9fa159bc03a1e4fb
abhi-shry <124462515+abhi-shry@users.noreply.github.com>
2023-08-18 10:40:53 +0000

    learned var, const, let etc

---
+++ 01_Basics/variables.js
@@ -0,0 +1,7 @@
+const empId="123";            //double quote optional 
+let empSalary;         //double quote optional + +  initialosed(optionl)
+
+var empAddress="Pune";        //double quote manadatory + NOt recommended to use due to scope issue + must be initialosed(else give error)
+empDesignation="Manager";     //double quote manadatory + must be initialosed(else give error)
+
+console.table([empId, empSalary, empAddress,empDesignation ]);
